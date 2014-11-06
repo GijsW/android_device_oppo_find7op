cf7c9177d2c55169d4ba3250c652b293cff9f59b
Gijs W <thisisnimrr@gmail.com>
2014-11-06 21:29:35 +0100

    DUify

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,4 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo omni_find7op-userdebug
-add_lunch_combo omni_find7op-eng
+add_lunch_combo du_find7op-userdebug
